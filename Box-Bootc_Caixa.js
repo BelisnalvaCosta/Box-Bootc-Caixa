7dde68b04790da50dfb29d8a754977e79e4179a2
BelisnalvaCosta <bella-costa@hotmail.com.br>
2024-11-15 13:04:21 -0300

    Committing script Box-Bootc_Caixa.js

---
+++ Box-Bootc_Caixa.js
@@ -0,0 +1,7 @@
+// Box-Bootc_Caixa.js
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('button');
+    button.addEventListener('click', () => {
+        alert('Cadastro realizado com sucesso!');
+    });
+});
